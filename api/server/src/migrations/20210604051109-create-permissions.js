b5f539c0660b3c34dac5d864358861942622a2fc
PPSCoders <kunzwangdi@gmail.com>
2021-06-11 10:30:32 +0600

    s

--- api/server/src/migrations/20210604051109-create-permissions.js
+++ api/server/src/migrations/20210604051109-create-permissions.js
@@ -10,7 +10,7 @@ module.exports = {
       },
       user_id: {
         type: Sequelize.INTEGER,
-        references: { model: 'users', key: 'id'}
+        references: { model: 'Users', key: 'id'}
       },
       permission: {
         type: Sequelize.STRING
